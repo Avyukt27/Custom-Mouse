c51c29bec1ae02baf24ab86c814dc4b965f207a5
Avyukt27 <avyukt.aggarwal007@gmail.com>
2025-10-15 11:52:51 +0530

    Added coloured stars

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const colours = ['#00d492', '#51a2ff', '#e12afb'];
+
+const selectRandom = (arr) => arr[Math.floor(Math.random() * arr.length)];
+
 document.addEventListener('mousemove', (event) => {
   const mouseX = event.clientX;
   const mouseY = event.clientY;
@@ -7,6 +11,13 @@ document.addEventListener('mousemove', (event) => {
   dot.style.left = `${mouseX}px`;
   dot.style.top = `${mouseY}px`;
   document.body.appendChild(dot);
-  
-  setTimeout(() => document.body.removeChild(dot), 50);
+  setTimeout(() => dot.remove(), 100);
+
+  const star = document.createElement('div');
+  star.className = 'star fa-solid fa-star';
+  star.style.left = `${mouseX}px`;
+  star.style.top = `${mouseY}px`;
+  star.style.color = selectRandom(colours);
+  document.body.appendChild(star);
+  setTimeout(() => star.remove(), 1500);
 })
